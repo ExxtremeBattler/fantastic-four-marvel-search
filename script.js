f47d064f875b956b48ca370b8d57c7399ceda726
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-08 16:00:02 +0000

    - after clicking 'search', the hero name is converted to id, and then a list of comics are returned to the console

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
+$( document ).ready(function(){ //makes sure html & css load before running the JS
+
+  let marvelPublicApiKey = "6f68ec270b01384876787724cd124e64"
+  let marvelPrivateApiKey = "189d5043bc54220212294812e2de32eeda0e263a"
+  let marvelAuthKey = marvelPublicApiKey + marvelPrivateApiKey
+  let marvelHash = "701330a00b13eb2a18e31cad8b72fe5b"
+  
+
+  
+
+
 // SEARCH
 // advance search options onclick handler
 $("#advanceBtn").on("click", function (event) {
   event.preventDefault();
   $(".search-options").removeClass("hide");
 });
+
+// starting the onClick function for 'Search'
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  let searchInput = $("#searchInput").val()
+  let marvelIdURL = "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" + searchInput + "&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b"
+
+  $.ajax({
+    url: marvelIdURL,
+    method: "GET"
+  }).then(function(response){
+
+    var characterId = response.data.results[0].id
+    console.log (characterId)
+      
+
+    let marvelComicURL = "https://gateway.marvel.com:443/v1/public/characters/" + characterId + "/comics?&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b"
+
+   
+
+    $.ajax({
+      url: marvelComicURL,
+      method: "GET"
+    }).then(function(response){
+
+      console.log(response.data.results);
+
+
+    });
+    
+
+   })
+    });
+  });
+
+
+
+
