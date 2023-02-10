4095f88052ce46e1750de7eaa904027b7739ce65
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-10 16:24:55 +0000

    userseaches array now deletes any duplicates

--- script.js
+++ script.js
@@ -78,6 +78,10 @@ $(document).ready(function () {
 
   function saveSearch(search) {
 
+    if (userSearches.includes(search)){
+      userSearches.splice(userSearches.indexOf(search), 1)
+    }
+
     userSearches.unshift(search);
     localStorage.setItem("marvelSearches", JSON.stringify(userSearches))
       
@@ -86,20 +90,59 @@ $(document).ready(function () {
       } 
       
       searchesList.empty()
-      // creates search history section/ buttons
 
+      
       for (let i = 0; i < userSearches.length; i++) {
+
         
         var searchButton = $ ("<button>")
         searchButton[0].innerHTML = userSearches[i]
+        console.log(searchButton)
+        console.log(userSearches)
 
         searchButton.on("click", function(event){
-          
-        })
+          event.preventDefault();
+
+    let searchInput = event.target.innerHTML
+    let marvelIdURL =
+      "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" +
+      searchInput +
+      "&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
+
+      saveSearch(searchInput)
+
+    // first ajax to turn the hero name in searchInput into an Id the API can use
+    $.ajax({
+      url: marvelIdURL,
+      method: "GET",
+    }).then(function (response) {
+      // store the first relevant character's id, log to be sure
+      var characterId = response.data.results[0].id;
+      console.log(characterId);
+
+      // logs all the relevant characters
+      console.log(response.data.results);
+
+      // new URL to search for comics of the chosen character
+      let marvelComicURL =
+        "https://gateway.marvel.com:443/v1/public/characters/" +
+        characterId +
+        "/comics?orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
+
+      // second ajax to return the last 20 or so comics for the character, will update to 5 tomorrow, check log for results
+      $.ajax({
+        url: marvelComicURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response.data.results);
+      });
+    });
+  })
+
         
-        let listItemWrapper = $("<li>")
-        listItemWrapper.append(searchButton)
-        searchesList.append(listItemWrapper)
+    let listItemWrapper = $("<li>")
+    listItemWrapper.append(searchButton)
+    searchesList.append(listItemWrapper)
       }
   }
 
@@ -140,9 +183,9 @@ $(document).ready(function () {
         method: "GET",
       }).then(function (response) {
         console.log(response.data.results);
-      });
-    });
-  });
+      })
+    })
+  })
 });
 
 let hero = {
@@ -183,5 +226,4 @@ $.ajax({
   $("#bio-group").text(hero.group)
   $("#bio-img").attr("src", hero.img)
   
-
 })
