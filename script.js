5d1fc5995c50d07c6df60d434b2d9031545215ac
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-11 17:18:18 +0000

    loadHistory() loads last search data, however: - buttons created by this function do not move to the top when clicked - searching a new term will empty the list and start a new one, as per saveSearch

--- script.js
+++ script.js
@@ -74,6 +74,61 @@ $(document).ready(function () {
 
   let searchesList = $("#searches-list")
   let userSearches = []
+  const storedSearches = JSON.parse(localStorage.getItem('marvelSearches'));
+
+
+  function loadSearches (){
+    
+    for (let i = 0; i < storedSearches.length; i++) {
+
+      var searchButton = $ ("<button>")
+      searchButton[0].innerHTML = storedSearches[i]
+
+      searchButton.on("click", function(event){
+        event.preventDefault();
+
+  let searchInput = event.target.innerHTML
+  let marvelIdURL =
+    "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" +
+    searchInput +
+    "&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
+
+  // first ajax to turn the hero name in searchInput into an Id the API can use
+  $.ajax({
+    url: marvelIdURL,
+    method: "GET",
+  }).then(function (response) {
+    // store the first relevant character's id, log to be sure
+    var characterId = response.data.results[0].id;
+    console.log(characterId);
+
+    // logs all the relevant characters
+    console.log(response.data.results);
+
+    // new URL to search for comics of the chosen character
+    let marvelComicURL =
+      "https://gateway.marvel.com:443/v1/public/characters/" +
+      characterId +
+      "/comics?orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
+
+    // second ajax to return the last 20 or so comics for the character, will update to 5 tomorrow, check log for results
+    $.ajax({
+      url: marvelComicURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response.data.results);
+    });
+  });
+})
+
+      
+  let listItemWrapper = $("<li>")
+  listItemWrapper.append(searchButton)
+  searchesList.append(listItemWrapper)
+    }
+  }
+
+  loadSearches()
 
 
   function saveSearch(search) {
