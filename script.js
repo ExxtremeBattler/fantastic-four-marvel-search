899053df30cbcd3e07a5bdf96a7f0ab7dbcdae44
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-11 17:48:48 +0000

    removed advanced search stuff, creating new branch for that

--- script.js
+++ script.js
@@ -105,23 +105,13 @@ $(document).ready(function () {
     // logs all the relevant characters
     console.log(response.data.results);
 
-    // grabs advanced search parameters
-    let year = $("#startYearInput").val
-    let series = $("#seriesInput").val
-    let creator = $("#creatorInput").val
-
-    
-
  
     // new URL to search for comics of the chosen character
 
     let marvelComicURL =
       "https://gateway.marvel.com:443/v1/public/characters/" +
       characterId +
-      "/comics?startYear="+ year + 
-      "&titleStartsWith=" + series +
-      "&creator = " + creator +
-      "&orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
+      "/comics?&orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
 
     // second ajax to return the last 20 or so comics for the character, will update to 5 tomorrow, check log for results
     $.ajax({
