a60a56491329a64005f989890f6155e6bcc0fe77
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-13 17:57:07 +0000

    started if-else logic for empty parameters

--- script.js
+++ script.js
@@ -235,28 +235,42 @@ $(document).ready(function () {
     let series = $("#seriesInput").val()
     let creator = $("#creatorInput").val()
 
-     console.log(year)
-     console.log(series);
+    console.log(year);
+    console.log(series);
+    console.log(creator);
 
-    
+    if (year === "" && series === "" && creator === ""){
 
-      //new URL to search for comics of the chosen character
+      let marvelComicURL =
+        "https://gateway.marvel.com:443/v1/public/characters/" +
+        characterId +
+        "/comics?orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
 
+    }
+    else if (!creator){
+      
       let marvelComicURL =
       "https://gateway.marvel.com:443/v1/public/characters/" +
       characterId +
       "/comics?startYear="+ year + 
       "&titleStartsWith=" + series +
       "&orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
-      // "&creator = " + 
-  
+     
+    }
+    
+    
+
+      //new URL to search for comics of the chosen character
 
       // let marvelComicURL =
-      //   "https://gateway.marvel.com:443/v1/public/characters/" +
-      //   characterId +
-      //   "/comics?orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
-
-
+      // "https://gateway.marvel.com:443/v1/public/characters/" +
+      // characterId +
+      // "/comics?startYear="+ year + 
+      // "&titleStartsWith=" + series +
+      //  "&creator = " + creator +
+      // "&orderBy=-onsaleDate&ts=1&apikey=6f68ec270b01384876787724cd124e64&hash=701330a00b13eb2a18e31cad8b72fe5b";
+     
+  
       // second ajax to return the last 20 or so comics for the character, will update to 5 tomorrow, check log for results
       $.ajax({
         url: marvelComicURL,
